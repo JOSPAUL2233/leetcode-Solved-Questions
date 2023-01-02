d4230db00f08a7042374b2da2aeeec7ce1743de5
JOSPAUL2233 <81140899+JOSPAUL2233@users.noreply.github.com>
2023-01-02 08:05:25 +0530

    Time: 5 ms (24.37%), Space: 6.1 MB (24.57%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        
+        if((word[0]>='A' && word[0]<='Z') && (word[1]>='A' && word[1]<='Z')){
+            
+            for(int i = 2;i<word.size();i++){
+                if(!(word[i]>='A' && word[i]<='Z'))return false;
+            }
+            
+        }else if((word[0]>='A' && word[0]<='Z') || (word[0]>='a' && word[0]<='z')){
+            
+            for(int i = 1;i<word.size();i++){
+                if(!(word[i]>='a' && word[i]<='z'))return false;
+            }
+            
+        }
+        
+        return true;
+        
+    }
+};
