d57a205a24d7c7407d347dc37e19b316be809a32
JOSPAUL2233 <81140899+JOSPAUL2233@users.noreply.github.com>
2023-01-17 23:07:28 +0530

    Time: 24 ms (71.00%), Space: 5.8 MB (79.62%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,32 +1,11 @@
 class Solution {
 public:
-    
-    void solve(long p,int n,bool &ans){
-        
-        if(p==n){
-        ans = true;
-        return;
-        }
-        if(p>n){
-            ans = false;
-            return;
-        }
-        
-        solve(p*3,n,ans);
-        
-        return;
-    }
+ 
     bool isPowerOfThree(int n) {
         
-        bool ans = false;
+       if(n <= 1) return n == 1;
         
-        if(n<=0)return false;
-        
-        if(n==1)return true;
-        
-        solve(3,n,ans);
-        
-        return ans;
+	return n % 3 == 0 && isPowerOfThree(n / 3);
         
     }
 };
