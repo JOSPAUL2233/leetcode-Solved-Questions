7035f1e9ba083cdddf1c56ed76ebd65740b0abea
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2023-01-30 17:33:09 +0530

    Time: 0 ms (100.00%), Space: 6 MB (16.48%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
@@ -2,7 +2,38 @@ class Solution {
 public:
     bool isStrictlyPalindromic(int n) {
                 
-      return false;
+        int base = 2;
+        string binary = "";
+        
+        while(base <= n-2){
+            
+            
+            binary = "";
+            int digit = n;
+            
+            while(digit){
+                
+                int rem = digit % base;
+                
+                digit /= base;
+                
+                binary += (to_string(rem));
+                
+            }
+            
+            //check whether it is palindrome
+            
+            for(int i = 0;i<binary.size()/2;i++){
+                
+                if(binary[i] != binary[binary.size()-1-i])return false;
+                
+            }
+            
+            base++;
+        
+        }
+        
+        return true;
          
     }
 };
