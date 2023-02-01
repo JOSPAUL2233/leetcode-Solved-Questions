5595983b2ba6906d94a0a50f37e11aa1039aeb85
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2023-02-01 18:35:15 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (65.71%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        
+        int len1 = str1.size();
+        int len2 = str2.size();
+        
+        if(str1+str2 != str2+str1)return "";
+        
+        return str1.substr(0,gcd(len1,len2));
+        
+    }
+};
