99a891a48386a7aebe0edbf78c5b1d2d2d92660b
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2023-02-13 13:31:59 +0530

    Added solution - LeetHub

---
+++ Diameter of a Binary Tree - GFG/diameter-of-a-binary-tree.cpp
@@ -0,0 +1,143 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child */
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+Node* newNode(int val) {
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    return temp;
+}
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+/* Tree node structure  used in the program
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+}; */
+
+class Solution {
+    
+    pair<int,int> dia(Node* root){
+        
+        if(root==NULL){
+            pair<int,int> p = make_pair(0,0);
+            return p;
+        }
+        
+        pair<int,int> leftSide = dia(root->left);
+        pair<int,int> rightSide = dia(root->right);
+        
+        int option1 = leftSide.first;
+        int option2 = rightSide.first;
+        int option3 = leftSide.second+rightSide.second+1;
+        
+        pair<int,int> p;
+        
+        p.first = max(option1,max(option2,option3));
+        p.second = max(leftSide.second,rightSide.second)+1;
+        return p;
+        
+    }
+    
+  public:
+    // Function to return the diameter of a Binary Tree.
+    int diameter(Node* root) {
+        
+        return dia(root).first;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+/* Driver program to test size function*/
+int main() {
+    int t;
+    scanf("%d\n", &t);
+    while (t--) {
+        string s;
+        getline(cin, s);
+        Node* root = buildTree(s);
+        Solution ob;
+        cout << ob.diameter(root) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
