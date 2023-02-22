9a2c266e1d98710baccff12914e3267e66059c6e
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2023-02-22 17:57:09 +0530

    Time: 113 ms (73.73%), Space: 28.7 MB (29.72%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -1,37 +1,62 @@
 class Solution {
 public:
     int numberOfBeams(vector<string>& bank) {
+      
+        //USING A VECTOR SPACE (ADDITIONAL)
         
+//         vector<int> beams;
         
-        vector<int> beams;
-        
-        for(int i = 0;i<bank.size();i++){
+//         for(int i = 0;i<bank.size();i++){
             
-            int count = 0;
-            string s = bank[i];
+//             int count = 0;
+//             string s = bank[i];
             
-            for(int j = 0;j<s.size();j++){
+//             for(int j = 0;j<s.size();j++){
                 
-                if(s[j]=='1')count++;
+//                 if(s[j]=='1')count++;
+                
+//             }
+            
+//             if(count!=0)beams.push_back(count);
+            
+            
+//         }
+//         int ans = 0;
+        
+//         if(beams.size()==0 || beams.size()==1)return 0;
+        
+        
+//         for(int i = 0;i<beams.size()-1;i++){
+            
+//             ans += beams[i]*beams[i+1];
+            
+//         }
+//         return ans;
+        
+        
+        //WITHOUT USING EXTRA SPACE
+        
+    int prev = 0;
+    int ans = 0;
+        for(string s: bank){
+            
+            int curr = 0;
+            
+            for(int i = 0;i<s.size();i++)
+                if(s[i]=='1')
+                    curr++;
+            
+            if(curr>0){
+                
+                ans += prev*curr;
+                prev = curr;
                 
             }
             
-            if(count!=0)beams.push_back(count);
-            
             
         }
-        int ans = 0;
         
-        if(beams.size()==0 || beams.size()==1)return 0;
-        
-        
-        for(int i = 0;i<beams.size()-1;i++){
-            
-            ans += beams[i]*beams[i+1];
-            
-        }
         return ans;
-        
         
     }
 };
