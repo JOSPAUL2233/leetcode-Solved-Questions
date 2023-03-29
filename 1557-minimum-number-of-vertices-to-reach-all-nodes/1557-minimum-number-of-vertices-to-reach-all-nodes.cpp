b097c5e8a328fbb818aae72dfeb4a181c8911c0a
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2023-03-29 19:57:31 +0530

    Time: 451 ms (13.99%), Space: 127.4 MB (8.94%) - LeetHub

---
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
+        
+        
+        //make adjacency list
+        
+        vector<list<int>> adj(n);
+        
+        for(int i = 0;i<edges.size();i++){
+            
+            int u = edges[i][0];
+            int v = edges[i][1];
+            
+            //since it is directed graph
+            adj[u].push_back(v);
+        }
+        
+        
+        //IDENTIFY THE VERTICES USING A HASHMAP
+        unordered_map<int,bool> vertices;
+        for(int i = 0;i<n;i++){      
+            for(auto j : adj[i]){              
+                vertices[j] = true;   
+            }  
+        }
+        
+        vector<int> ans;
+        
+        //extract the vertices
+        for(int i = 0;i<n;i++){
+            
+            if(!vertices[i])ans.push_back(i);
+            
+        }
+        
+        return ans;
+        
+    }
+};
