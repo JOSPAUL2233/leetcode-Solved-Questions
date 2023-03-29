4f8b6302ce7af748e00d78f6eb0711d3288d9aad
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2023-03-29 18:31:08 +0530

    Added solution - LeetHub

---
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
@@ -0,0 +1,103 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+    
+    void bfsCheck(int i,vector<int> adj[],unordered_map<int,bool> &visited,
+                    unordered_map<int,int> &parent,bool &ans){
+        
+            queue<int> que;
+            que.push(i);
+            
+            while(!que.empty()){
+                
+                int front = que.front();
+                que.pop();
+                
+                for(auto j : adj[front]){
+                    
+                    // cout<<"inside for loop : "<<j<<endl;
+                    
+                    if(!visited[j]){
+                        
+                        // cout<<"1st if : "<<j<<endl;
+                        
+                        visited[j] = true;
+                        parent[j] = front;
+                        que.push(j);
+                        
+                    }else if(visited[j] && (parent[front] != j)){
+                        
+                        //if visited and parent is not front
+                        
+                        // cout<<"2st if : "<<j<<endl<<endl;
+                        ans = true;
+                        return;
+                        
+                    }
+                    
+                }
+                
+            }
+            
+            ans = false;
+            return;
+        
+    }
+    
+  public:
+    // Function to detect cycle in an undirected graph.
+    bool isCycle(int V, vector<int> adj[]) {
+        
+        //make two maps - one for parent and other for visited
+        
+        unordered_map<int,int> parent;
+        unordered_map<int,bool> visited;
+        
+        bool ans = false;
+        
+        for(int i = 0;i<V;i++){
+            
+            if(!visited[i]){
+                visited[i] = true;
+                parent[i] = -1;
+                bfsCheck(i,adj,visited,parent,ans);
+                
+            }
+            
+            if(ans == true)return true;
+            
+        }
+        
+        return false;
+        
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+        vector<int> adj[V];
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        Solution obj;
+        bool ans = obj.isCycle(V, adj);
+        if (ans)
+            cout << "1\n";
+        else
+            cout << "0\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
