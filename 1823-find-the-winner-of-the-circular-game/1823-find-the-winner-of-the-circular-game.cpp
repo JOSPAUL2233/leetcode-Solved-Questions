a233b7bf953c650a41fa18d23b185259668e2db0
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2023-04-29 19:38:51 +0530

    Time: 8 ms (44.18%), Space: 6.4 MB (35.14%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,39 +1,38 @@
 class Solution {
-public:
-    int helper(ListNode *head,int k){
-        ListNode *temp=head;  //For traversing through the List and delete every kth Node 
-        while(head->next!=head){   //Till the last Node remains in the list i.e. the winner
-            ListNode *pre=NULL;
-            for(int i=1; i<k; i++){
-                pre=temp;
-                temp=temp->next;
-            }
-            //Deletion procedure
-            if(temp==head)
-                head=head->next;
-            pre->next=temp->next;
-            delete(temp);
-            temp=pre->next;
-        }
-        //The last node remaining becomes the head of the Circular Linked List i.e. Winner
-        return head->val;
+    
+    void solve(vector<int> &players,int start,int k){
+        
+        if(players.size()==1)return;
+        
+        start = start + k;//-------------chooseing next position
+        
+        start = start % players.size();//------making sure that it does not overflow the size
+        
+        players.erase(players.begin()+start); //-----excecute the player
+        
+        solve(players,start,k); //go for next iteration from next player
+        
     }
+    
+public:
     int findTheWinner(int n, int k) {
-        if(k==1)
-            return n;
-        //Designing Circular Linked List for Node 1,2,3,4,.....,n
-        ListNode *head=NULL,*tail=NULL;
-        for(int i=1; i<=n; i++){
-            ListNode *node=new ListNode(i);
-            if(head==NULL)
-                head=tail=node;
-            else{
-                tail->next=node;
-                tail=node;
-            }
-            tail->next=head;
+        
+        vector<int> players;
+        
+        int start = 0;
+        
+        for(int i = 0;i<n;i++){
+            
+            players.push_back(i+1);
+            
         }
-        //Helper function will eliminate the nodes and give us the winner
-        return helper(head,k);
+        
+        
+        k--;
+        solve(players,start,k);
+        
+        
+        return players[0];
+        
     }
 };
