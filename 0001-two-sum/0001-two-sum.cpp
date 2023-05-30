47e9a2e93c5932bdb80a8a185909cb901d3495d4
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2023-05-30 20:44:27 +0530

    Time: 14 ms (66.42%), Space: 10.9 MB (35.30%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        
     std::unordered_map<int, int> tmp;
+        
     for (int i = 0; i < nums.size(); ++i) {
-      if (tmp.count(target - nums[i])) {
-        return {tmp[target - nums[i]], i};
-      }
-      tmp[nums[i]] = i;
+        
+          int ele = target-nums[i];
+        
+          if ( tmp.count(ele) ){
+
+            return {tmp[ele], i};
+
+          }
+
+          tmp[nums[i]] = i;
+        
     }
+        
     return {-1, -1};
+        
   }
 };
