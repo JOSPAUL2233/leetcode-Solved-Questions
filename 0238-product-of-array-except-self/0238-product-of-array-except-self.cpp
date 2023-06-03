0953ecaaac6a6f2a507e992c38d2e1b640989257
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2023-06-03 15:50:38 +0530

    Time: 31 ms (43.40%), Space: 25.1 MB (18.56%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -2,52 +2,48 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         
+        vector<int> prefix(nums.size(),0);
+        vector<int> postfix(nums.size(),0);
+        vector<int> ans(nums.size(),0);
+        
         int product = 1;
-        int zeros = 0;
-        int zeroIndex = -1;
         
-        for(auto i : nums){
+        //assign the prefix values as the
+        //product of left elements
+        for(int i = 0;i<nums.size();i++){
             
-            if(i==0)zeros++;
-            product*=i;
+            product *= nums[i];
+            
+            prefix[i] = product;
             
         }
         
-        if(product == 0){
+        product = 1;
+        
+        //assign the postfix values as the
+        //product of right elements
+        for(int i = nums.size()-1;i>=0;i--){
             
-            vector<int> ans(nums.size(),0);
+            product *= nums[i];
             
-           if(zeros>1){
-               return ans;
-           }else{
-               
-               int prod = 1;
-               
-             for(int i = 0;i<nums.size();i++){
-                 if(nums[i]==0)zeroIndex = i;
-                 else 
-                     prod = prod*nums[i];
-             }
-             ans[zeroIndex] = prod;
-             return ans;
-               
-           }
-            
-        }else{
-            
-             
-            vector<int> ans;
-
-            for(int i = 0;i<nums.size();i++){
-
-                ans.push_back(product/nums[i]);
-
-            }
-
-            return ans;
-
+            postfix[i] = product;
             
         }
-       
+        
+        //now multiply the left and right of the 
+        //prefix and postfix array to find the
+        //product except the current element
+        
+        //calculate for 1st and last element
+        ans[0] = postfix[1];
+        ans[nums.size()-1] = prefix[nums.size()-2];
+        
+        for(int i = 1;i<nums.size()-1;i++){
+            
+            ans[i] = prefix[i-1] * postfix[i+1];
+            
+        }
+        
+        return ans;
     }
 };
