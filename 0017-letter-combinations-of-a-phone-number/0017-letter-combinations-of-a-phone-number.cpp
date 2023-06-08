9baba04730b076b22a84f3d5dfbb6fc41b80b0be
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2023-06-08 20:13:35 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (7.35%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -3,20 +3,21 @@ class Solution {
     void solve(vector<string> mapping,string digits,string path,
                 vector<string> &ans, int index){
         
-        if(index == digits.size()){
-            
+        //when answer is ready
+        if(index == digits.size()){     
             ans.push_back(path);
             return ;
-            
         }
         
+        //extract the corresponding string letters
         string letters = mapping[digits[index] - '0'];
-                
+        
+        //iterate through all the string letters
         for(int i = 0;i<letters.size();i++){
             
-            path.push_back(letters[i]);
+            path.push_back(letters[i]);//add the element and do recursion
             solve(mapping,digits,path,ans,index+1);
-            path.pop_back();
+            path.pop_back();//remove the currently added element while going back
             
         }
         
@@ -28,12 +29,12 @@ public:
         //edge case
         if(digits.size()==0)return{};
         
+        //make a mapping for numbers and it's letters
         vector<string> mapping = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
         vector<string> ans;
         string path;
         
         int index = 0;
-        
         solve(mapping,digits,path,ans,index);
         
         return ans;
