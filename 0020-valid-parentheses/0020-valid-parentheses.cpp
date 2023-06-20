3518a0c429c8b1b9d9503c94f9f4abeb16bbbe18
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2023-06-20 13:14:28 +0530

    Time: 4 ms (40.32%), Space: 6.3 MB (40.16%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -4,39 +4,42 @@ public:
         
         stack<char> st;
         
-        for(auto x:s){
+        for(int i = 0;i<s.size();i++){
             
-            if(x=='[' || x=='{' || x=='('){
+            char ele = s[i];
+            
+            //if element is any opening bracket, directly push
+            //it into the stack
+            if(ele == '(' || ele == '{' || ele == '['){
                 
-                st.push(x);
-               
+                st.push(ele);
                 
             }else{
-                
-                if(st.empty())return false;
-                
-                char temp = st.top();
-                
-                if( (x==']' && temp=='[') || (x=='}' && temp=='{') || (x==')' && temp=='(') ){
-                    
-                    
-                    st.pop();
-                    
-                }else{
-                    
+           
+                //stack is empty and there is a closing bracket
+                //which directly says that this can't be balanced.
+                //because it won't be having any matching opening bracket.
+                if(st.empty())
                     return false;
-                }
-            
-            }
-            
-            
+                
+                //correct order
+                if(( ele == ')' && st.top() == '(' ) || ( ele == ']' && st.top() == '[' )
+                                                   ||( ele == '}' && st.top() == '{' )){
+                    st.pop();
+                                                       
+                }else{//wrong order
+                                                       
+                    return false;
+                                                       
+                } 
+            }   
         }
         
-       
-        
-        if(st.empty())return true;
-        else return false;
-        
+        //stack should be empty(all brackets should be cancelled with other pair)
+        if(st.empty())
+            return true;
+        else
+            return false;
         
     }
 };
