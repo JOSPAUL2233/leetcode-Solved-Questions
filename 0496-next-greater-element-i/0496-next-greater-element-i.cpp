29f35e77e090504a412942e44aacee352106e580
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2023-06-22 11:22:04 +0530

    Time: 53 ms (6.54%), Space: 8.5 MB (85.24%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -2,43 +2,45 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         
-        vector<int> greater(nums2.size(),-1);
-        vector<int> ans(nums1.size());
-        
         stack<int> st;
+        vector<int> nextGreater(nums2.size(),0);
         
-        for(int i = nums2.size()-1;i>=0;i--){
+        for(int i=nums2.size()-1 ; i>=0 ; i--){
             
-            
-            while(!st.empty()){
-                
-                if(st.top()>nums2[i]){
-                    
-                    greater[i] = st.top();
-    
-                    break;    
-                }
-                
+            //remove all the smaller elements to the right from stack
+            while(!st.empty() && st.top() <= nums2[i]){
                 st.pop();
             }
             
+            //update the answer
+            if(st.empty()){
+                nextGreater[i] = -1;
+            }else{
+                nextGreater[i] = st.top();
+            }
+            
+            //push the current element into the stack
             st.push(nums2[i]);
             
         }
         
+        //fetch the nextGreater elements which are needed (given in arr1)
+        vector<int> fetched;
         
         for(int i = 0;i<nums1.size();i++){
             
             for(int j = 0;j<nums2.size();j++){
                 
-                if(nums1[i] == nums2[j])ans[i] = greater[j];
+                if(nums1[i] == nums2[j]){
+                    
+                    fetched.push_back(nextGreater[j]);
+                    
+                }
                 
             }
             
         }
-      
-        return ans;
         
-        
+        return fetched;
     }
 };
