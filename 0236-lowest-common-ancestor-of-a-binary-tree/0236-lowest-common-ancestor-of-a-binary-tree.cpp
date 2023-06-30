eab2be5a8869c2788ae7c6d771b4dcb7fe7c72a8
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2023-06-30 22:36:15 +0530

    Time: 19 ms (60.38%), Space: 14.4 MB (23.88%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -11,17 +11,26 @@ class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         
-        if(root == NULL)return NULL;
+        if(root == NULL)
+            return NULL;
         
-        if(root == p || root==q)return root;
+        //if it reaches any of the node
+        if(root == p || root == q)
+            return root;
         
-        TreeNode* leftSide = lowestCommonAncestor(root->left,p,q);
-        TreeNode* rightSide = lowestCommonAncestor(root->right,p,q);
+        //go and get the left and right side nodes if it is found
+        TreeNode* leftSide = lowestCommonAncestor(root->left, p, q);
+        TreeNode* rightSide = lowestCommonAncestor(root->right, p, q);
         
-        if(leftSide && !rightSide)return leftSide;
-        else if(rightSide && !leftSide)return rightSide;
-        else if(rightSide && leftSide)return root;
-        else return NULL;
+        //conditions for returning
+        if(leftSide && rightSide)
+            return root;
+        else if(leftSide && !rightSide)
+            return leftSide;
+        else if(rightSide && !leftSide)
+            return rightSide;
+        else//when leftSide and rightSide both are NULL
+            return NULL;
         
     }
 };
