aa1380c8c2297e33bd75eeacaaf59c3afdff9590
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2023-07-14 12:39:44 +0530

    Time: 20 ms (68.86%), Space: 17.9 MB (40.85%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,14 +2,19 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         
+        //here, zero indexing is followed
+        //means, the numbers are from 0 to n
+        
         int n = nums.size();
         int ans = 0;
         
         for(int i = 0;i<n;i++){
             
+            //XORing i and the element
             ans ^= (nums[i]^i);
             
         }
+        
         
         ans^=n;
         
