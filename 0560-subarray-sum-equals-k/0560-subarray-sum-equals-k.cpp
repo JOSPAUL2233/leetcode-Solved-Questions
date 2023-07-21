405d62c0679413c2510491ed327c282eb6379f6b
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2023-07-21 22:54:12 +0530

    Time: 96 ms (80.05%), Space: 41.3 MB (92.03%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -2,26 +2,31 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         
-        unordered_map<int,int> prefixSums;
+        unordered_map<int,int> prefixSums;//(sum, number or occurances)
         
         int sum = 0;
         int counter = 0;
         
+        //stores the prefixSum
         prefixSums[sum]++;
         
         for(int i = 0;i<nums.size();i++){
             
             sum += nums[i];
             
+            //get the rem
             int rem = sum - k;
         
-            
+            //check if rem is already there in the prefixSum
+            //if yes, then that means that a subarray is there with the calculated sum
             if(prefixSums.find(rem) != prefixSums.end()){
                 
                 counter = counter + prefixSums[rem];
                 
             }
             
+            //increment the occurance of the prefixSum
+            //of the current sum
             prefixSums[sum]++;
             
         }
