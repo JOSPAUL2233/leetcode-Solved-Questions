62a4b7558c1612d58fdf7394dfbd00b123b3d992
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2023-07-22 17:45:21 +0530

    Added solution - LeetHub

---
+++ Largest subarray with 0 sum - GFG/largest-subarray-with-0-sum.cpp
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+/*You are required to complete this function*/
+
+class Solution{
+    public:
+    int maxLen(vector<int>&A, int n)
+    {   
+        
+        //we will store prefixSum
+        map<int,int> prefixSum;
+        
+        int sum = 0;
+        int maxLength = INT_MIN;
+
+        for(int i = 0;i<n;i++){
+            
+            sum += A[i];
+            
+            //find the element removing which we get 0
+            int rem = sum;
+            
+            if(sum == 0){
+                maxLength = max(maxLength,i+1);
+            }
+
+            //check if that element is found already
+            if(prefixSum.find(rem) != prefixSum.end()){
+                
+                // cout<<"inside"<<endl;
+                maxLength = max(maxLength , (i - prefixSum[rem]));
+                
+            }
+            
+            // cout<<"sum "<<sum<<endl;
+            
+            //store only if the sum is not present anywhere left side
+            //since we have to find the maximum length
+            if(prefixSum.find(sum) == prefixSum.end())
+                prefixSum[sum] = i;
+                
+            // cout<<"prefix "<<prefixSum[sum]<<endl;
+            
+        }
+        
+        if(maxLength == INT_MIN)
+            return 0;
+        else
+            return maxLength;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int m;
+        cin>>m;
+        vector<int> array1(m);
+        for (int i = 0; i < m; ++i){
+            cin>>array1[i];
+        }
+        Solution ob;
+        cout<<ob.maxLen(array1,m)<<endl;
+    }
+    return 0; 
+}
+
+
+
+// } Driver Code Ends
