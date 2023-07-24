de01a6588c2159cea1f90ee08a87d1c5f722f96c
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2023-07-24 13:28:29 +0530

    Added solution - LeetHub

--- Find Missing And Repeating - GFG/find-missing-and-repeating.cpp
+++ Find Missing And Repeating - GFG/find-missing-and-repeating.cpp
@@ -8,24 +8,45 @@ class Solution{
 public:
     vector<int> findTwoElement(vector<int> arr, int n) {
         
-        vector<int> hashArr(n+1,0);
-       
-       int repeat = -1;
-       int missing = -1;
-       
-       for(int i = 0;i<n;i++){
-           
-           hashArr[arr[i]]++;
-           
-       }
-       for(int i = 0;i<hashArr.size();i++){
-           
-           if(hashArr[i]==2)repeat = i;
-           if(hashArr[i]==0 && i!=0)missing = i;
-           
-       }
-       
-       return {repeat,missing};
+        //get sumArr and sqSumArr from the array
+        
+        long long sumArr = 0;
+        long long sqSumArr = 0;
+        
+        for(int i = 0;i<n;i++){
+            
+            sumArr += (long long)arr[i];
+            sqSumArr += ((long long)arr[i]*arr[i]);
+            
+        }
+        
+        //get sumAp and sqSumAp using formula
+        //sum of n natureal numbers = n(n+1)/2
+        //sum of squares of n natureal number =( n(n+1)(2n+1) )/ 6
+        
+        long long sumAp = (long long)n*(n+1)/2;
+        long long sqSumAp = ((long long)n*(n+1)*(2*n+1))/6;
+        
+        
+        //let x be repeating element and y be missing element
+        //now do sumArr - sumAp to get x - y and store it in ele1
+        //and do sqSumArr - sqSumAp to get x^2 - y^2 and store it in ele2
+        long long ele1 = sumArr - sumAp;
+        long long ele2 = sqSumArr - sqSumAp;
+        
+        //put divide ele2 from using ele1 to get x + y and store it in ele3
+        long long ele3 = ele2 / ele1;
+        
+        //now, we have got ele1 as x-y and ele3 as x+y
+        
+        //by adding x-y and x+y , we will get 2x, then divide it by 2 to
+        //get the value of x
+        int x = (ele1 + ele3)/2;
+        
+        //subtract x from x + y(ie. ele3) to get the value of y
+        int y = ele3 - x;
+        
+        return {x,y};
         
     }
 };
