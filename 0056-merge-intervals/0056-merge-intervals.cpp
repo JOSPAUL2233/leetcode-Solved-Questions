d2fb874c381240df0489d20469c4e439271d9fcd
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2023-07-24 11:58:37 +0530

    Time: 41 ms (72.14%), Space: 18.9 MB (72.38%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -4,23 +4,32 @@ public:
         
         vector<vector<int>> ans;
     
+        //sort the vector
         sort(intervals.begin(),intervals.end());
         
+        //make a p for current interval and push the first elements into it
         vector<int> p;
         p.push_back(intervals[0][0]);
         p.push_back(intervals[0][1]);
     
-        
+        //start iterating from the second vector
         for(int i = 1;i<intervals.size();i++){
             
+            //if the first element in the new interval is lesser than the
+            //second element in the pair called p
             
-            if(intervals[i][0] <= p[1]){
+            //and check if the second element in the pair is smaller than the
+            //second element in new interval
+            
+            if(intervals[i][0] <= p[1]){ //overlapping happening
                 
-                p[1] = max(p[1],intervals[i][1]);
-    
+                p[1] = max(p[1] , intervals[i][1]);
                 
-            }else{
+            }else{//no overlapping
                 
+                    //if the interval did not find overlapping,
+                    //push the current p and start finding next p from
+                    //the current position
                     ans.push_back(p);
                 
                     p[0] = intervals[i][0];
