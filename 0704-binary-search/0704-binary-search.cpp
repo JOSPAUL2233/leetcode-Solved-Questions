3f1ffae7287158201d651964f129f703044e2769
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2023-07-25 23:37:43 +0530

    Time: 42 ms (59.04%), Space: 27.6 MB (32.79%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,25 +1,45 @@
 class Solution {
 public:
+    
+    int binarySearch(vector<int> &nums,int target,int start,int end){
+        
+        if(start > end)
+            return -1;
+        
+        int mid = start +(end - start)/2;
+        cout<<mid<<endl;
+        
+        if(nums[mid] == target)
+            return mid;
+        
+        if(nums[mid] > target)
+            return binarySearch(nums,target,start,mid-1);
+        else
+            return binarySearch(nums,target,mid+1,end);
+        
+    }
     int search(vector<int>& nums, int target) {
         
         int start = 0;
         int end = nums.size()-1;
         
-        while(start <= end){
+//         while(start <= end){
             
-            int mid = start + (end - start)/2;
+//             int mid = start + (end - start)/2;
             
-            if(nums[mid] == target)
-                return mid;
+//             if(nums[mid] == target)
+//                 return mid;
             
-            if(nums[mid] < target)
-                start = mid+1;
-            else
-                end = mid-1;
+//             if(nums[mid] < target)
+//                 start = mid+1;
+//             else
+//                 end = mid-1;
             
-        }
+//         }
         
-        return -1;
+        int ans = binarySearch(nums,target,start,end);
+        
+        return ans;
         
     }
 };
