c61c69ec2a52fdeda3c8f7c1e1ee2b46e8df8752
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2023-08-01 23:39:11 +0530

    Time: 3 ms (86.10%), Space: 8.9 MB (49.47%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -2,20 +2,42 @@ class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
         
-        if(nums.size()==1)
-            return 0;
+        //using binary search-------------------------
         
-        if(nums[0] > nums[1])
-            return 0;
+        //take care of edge cases
         
-        if(nums[nums.size()-1] > nums[nums.size()-2])
-            return nums.size()-1;
+        int n = nums.size();
         
-        //linear search
-        for(int i = 1;i<nums.size()-1;i++){
+        if(n == 1) return 0; // single element
+        
+		// check if 0th/n-1th index is the peak element
+        if(nums[0] > nums[1]) return 0;
+        if(nums[n-1] > nums[n-2]) return n-1;
+        
+        
+        //there are three conditions-------------------------
+        //1. mid element is the peak element
+        //2. mid element is greater than prev element
+        //3. mid element is lesser than prev element or lesser than next element and prev
+        //   element
+        
+        int low = 1;
+        int high = nums.size()-2;
+        
+        while(low <= high){
             
-            if(nums[i] > nums[i-1] && nums[i] > nums[i+1])
-                return i;
+            int mid = low + (high - low)/2;    
+            
+            //peak is found
+            if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1])
+                return mid;
+
+            //peak exists in the left part
+            if((mid-1) >= 0 && nums[mid] > nums[mid-1])
+                low = mid+1;
+            //peak exists in the right part
+            else
+                high = mid-1;
             
         }
         
