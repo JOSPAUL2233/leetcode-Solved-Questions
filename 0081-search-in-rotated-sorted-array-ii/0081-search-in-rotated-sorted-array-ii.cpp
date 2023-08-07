7daf0749ac2ddbdfda463ebff9b4c491af58a457
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2023-08-07 14:09:48 +0530

    Time: 9 ms (27.01%), Space: 14.2 MB (17.60%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        
+        int low = 0;
+        int high = nums.size()-1;
+        
+        while(low <= high){
+            
+            int mid = low + (high - low)/2;
+            
+            //if we got the target
+            if(nums[mid] == target)
+                return true;
+            
+            //check if low,mid and high elements are equal,if yes,
+            //shrink the width and continue for next iteration
+            if(nums[low] == nums[mid] && nums[mid] == nums[high]){
+                
+                low++;
+                high--;
+                
+                continue;
+                
+            }
+            
+            //if left part is sorted
+            if(nums[low] <= nums[mid]){
+                
+                //check if target is in the left part,then search in left part
+                //otherwise search in the right part
+                if(target >= nums[low] && target <= nums[mid]){
+                    high = mid-1;
+                }else{
+                    low = mid+1;
+                }
+                
+                
+            }else{//if right part is sorted
+                
+                //check if target is in the right part,then search in right part
+                //otherwise search in the left part
+                if(target >= nums[mid] && target <= nums[high]){
+                    low = mid+1;
+                }else{
+                    high = mid-1;
+                }
+                
+            }
+            
+            
+        }
+        
+        return false;
+        
+    }
+};
