82f694ce4df42b060f60c08fdaa3ac1764889cea
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2023-08-16 19:20:57 +0530

    Time: 15 ms (30.62%), Space: 9.1 MB (10.82%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    
+    string repeatChar(char c,int size){
+        
+        string s = "";
+        
+        for(int i = 0;i<size;i++)
+            s += c;
+        
+        return s;
+        
+    }
+    
+    string frequencySort(string s) {
+        
+        //count frequency
+        map<char,int> freq;
+        
+        for(char c : s){
+            
+            freq[c]++;
+            
+        }
+        
+        
+        //push freq into heap based on frequency
+        priority_queue<pair<int,char> > que;
+        
+        for(auto p : freq){
+            
+            que.push({p.second,p.first});
+            
+        }
+        
+        //now, place the elements into new string after sorting characters by freq
+        string str = "";
+        while(!que.empty()){
+            
+            //store char frequency times
+            str += repeatChar( que.top().second , que.top().first );
+            que.pop();
+            
+        }
+        
+        return str;
+    }
+};
