aef675a6f1cdb24e0dfc3a061f5d74fcb6727f29
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2023-08-16 18:27:00 +0530

    Time: 9 ms (70.07%), Space: 7.3 MB (82.54%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,31 +2,11 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         
-        if(s.size() != t.size())
-            return false;
+        //sort both the strings
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
         
-        //maintain hashArray for the letter freq of s and also for t
-        
-        vector<int> first(26,0);
-        vector<int> second(26,0);
-            
-        for(int i = 0;i<s.size();i++){
-            
-            first[ s[i] - 'a' ]++;
-            
-            second[ t[i] - 'a' ]++;
-            
-        }
-        
-        //check whether the frequencies are same
-        for(int i = 0;i<26;i++){
-            
-            if(first[i] != second[i])
-                return false;
-            
-        }
-        
-        return true;
+        return s==t;
         
     }
 };
