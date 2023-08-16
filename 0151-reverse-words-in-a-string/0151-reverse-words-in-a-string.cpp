06aba478a646d40bac2fcadeaae277a1789a2335
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2023-08-16 16:22:16 +0530

    Time: 14 ms (15.18%), Space: 20.8 MB (27.27%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    
+    void reverse(string& s,int left,int right){
+        
+        // for(int i = left;i<=right;i++)
+        //     cout<<s[i];
+        
+        while(left < right){
+            
+            swap(s[left],s[right]);
+            
+            left++;
+            right--;
+        }
+        
+    }
+    
+    string reverseWords(string s) {
+        
+        //INTUITION
+        //reverse the whole string and then reverse word by word
+        
+        int i = 0;
+        
+        string str = "";
+        
+        int n = s.size();
+        
+        while(i < n){
+
+            //find the 1st char
+            while(i < n && s[i] == ' ')i++;
+            
+            if(i >= n)
+                break;
+
+            //find the last char
+            int j = i+1;
+            while(j < n && s[j] != ' ')j++;
+
+            string word = s.substr(i,j-i);
+            
+            if(str.size() == 0)
+                str = word;
+            else
+                str = word + " " + str;
+            
+            i = j+1;
+            
+        }
+        
+        return str;
+        
+    }
+};
