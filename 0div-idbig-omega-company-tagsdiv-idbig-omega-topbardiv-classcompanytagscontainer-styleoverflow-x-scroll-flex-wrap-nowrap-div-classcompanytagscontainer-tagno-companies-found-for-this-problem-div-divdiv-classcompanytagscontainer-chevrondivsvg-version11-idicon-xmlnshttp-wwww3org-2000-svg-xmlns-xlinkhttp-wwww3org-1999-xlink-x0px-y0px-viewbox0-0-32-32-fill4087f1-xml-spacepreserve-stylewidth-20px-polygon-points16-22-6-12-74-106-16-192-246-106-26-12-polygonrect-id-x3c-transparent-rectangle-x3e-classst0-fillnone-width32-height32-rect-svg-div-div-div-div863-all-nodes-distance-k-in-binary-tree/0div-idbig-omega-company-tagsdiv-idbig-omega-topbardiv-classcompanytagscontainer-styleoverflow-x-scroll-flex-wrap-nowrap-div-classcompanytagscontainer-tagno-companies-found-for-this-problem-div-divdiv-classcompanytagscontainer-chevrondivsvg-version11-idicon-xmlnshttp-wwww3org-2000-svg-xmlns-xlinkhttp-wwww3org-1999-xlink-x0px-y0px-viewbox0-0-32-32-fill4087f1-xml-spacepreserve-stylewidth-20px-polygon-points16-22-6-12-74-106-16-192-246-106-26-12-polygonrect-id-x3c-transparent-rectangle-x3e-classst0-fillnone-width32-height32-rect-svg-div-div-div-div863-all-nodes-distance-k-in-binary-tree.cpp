cd1efa330d14395e150964e56fd4dd192b5077ae
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2023-10-17 23:11:13 +0530

    Time: 3 ms (83.28%), Space: 14.2 MB (21.54%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div863-all-nodes-distance-k-in-binary-tree/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div863-all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,130 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    
+    void storeParent(TreeNode* root,map<TreeNode*,TreeNode*> &parent){
+        
+        queue<TreeNode*> que;
+        
+        que.push(root);
+        parent[root] = NULL;
+        
+        while(!que.empty()){
+            
+            int size = que.size();
+            
+            for(int i = 0;i<size;i++){
+                
+                TreeNode* node = que.front();
+                que.pop();
+                
+                //check for left child
+                if(node->left){
+                    
+                    parent[node->left] = node;
+                    que.push(node->left);
+                    
+                }
+                
+                //check for right child
+                if(node->right){
+                    
+                    parent[node->right] = node;
+                    que.push(node->right);
+                    
+                }
+                
+            }
+            
+        }
+        
+    }
+    
+    vector<int> getNodes(TreeNode* root,TreeNode* target,int k,map<TreeNode*,TreeNode*> parent){
+
+        //keep a track of visited nodes
+        map<TreeNode*,bool> visited;
+        
+        queue<TreeNode*> que;
+        que.push(target);
+        visited[target] = true;
+        
+        //counts the levels
+        int level = 0;
+        vector<int> ans;
+        
+        while(!que.empty()){
+            
+            int size = que.size();
+                        
+            for(int i = 0;i<size;i++){
+                
+                TreeNode* node = que.front();
+                que.pop();
+                
+                //k distance
+                if(level == k)
+                    ans.push_back(node->val);
+                
+                //upward direction
+                if(parent[node] && !visited[parent[node]]){
+                    
+                    que.push(parent[node]);
+                    visited[parent[node]] = true;
+                    
+                }
+                
+                //left direction
+                if(node->left && !visited[node->left]){
+                    
+                    que.push(node->left);
+                    visited[node->left] = true;
+                    
+                }
+                
+                //down direction
+                if(node->right && !visited[node->right]){
+                    
+                    que.push(node->right);
+                    visited[node->right] = true;
+                    
+                }
+                
+            }
+            
+            //increment level
+            level++;
+            
+            //break if level crosses k
+            if(level > k)
+                break;
+            
+        }
+        
+        return ans;
+        
+    }
+    
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        
+        //keep a track of parent
+        map<TreeNode*,TreeNode*> parent;
+        
+        //do a BFS for storing parent
+        storeParent(root,parent);
+        
+        //do a BFS for finding k distanced nodes
+        vector<int> ans = getNodes(root,target,k,parent);
+        
+        return ans;
+        
+    }
+};
