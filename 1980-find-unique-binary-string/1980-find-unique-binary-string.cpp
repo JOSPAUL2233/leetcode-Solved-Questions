5defae98c3bebfda9a492f75ddc0d67daa59a55b
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2023-11-16 15:10:03 +0530

    Time: 17 ms (22.51%), Space: 24.4 MB (17.07%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -1,41 +1,57 @@
 class Solution {
 public:
     
-    void solve(int num, string path,vector<string> nums,string &ans){
+    string getString(int index,string path, vector<string> nums,set<string> st){
         
-        if(ans.size() != 0)
-            return;
-        
-        //base case
-        if(path.size() == num){
+        //base case will be reached when the index exceeds the last index of nums
+        if(index == nums.size()){
             
-            //check if path is there in the 
-            for(int i = 0;i<nums.size();i++){
-                
-                if(nums[i] == path)
-                    return;
-                
-            }
-            
-            ans = path;
-            return;
+            if(st.find(path) == st.end())//if the answer is found
+                return path;
+            else//if the answer is not found
+                return "";
             
         }
         
-        solve(num,path+'0',nums,ans);//recursive call
+        //recursive calls
         
-        solve(num,path+'1',nums,ans);//recursive call
+        //try taking 0 in the path and go for further calls---------------
+        path.push_back('0');
+        string str1 = getString(index+1,path,nums,st);
         
+        //if we cound find the answer, return that answer itself
+        if(str1.size() > 0)
+            return str1;
+        
+        //if we cound'nt find the answer,remove 0 and try putting 1 inplace of 0
+        //and go for further calls
+        path.pop_back();
+        path.push_back('1');
+        
+        string str2 = getString(index+1,path,nums,st);
+        
+        //if we cound find the answer, return that answer itself
+        if(str2.size() > 0)
+            return str2;
+        
+        //if non of str1 or str2 gave the answer, return an empty string
+        return "";
     }
     
     string findDifferentBinaryString(vector<string>& nums) {
         
-        string path = "";
+        //push all the elements to a hashSet so to check for the answer further
+        set<string> st;
+        for(string s: nums)
+                st.insert(s);
         
-        string ans = "";
+        //do generate recursively all the possible binary strings with the same length
+        //and get the one which is not present in set
+        int index = 0;
         
-        solve(nums[0].size(),path,nums,ans);
-        
+        //this gets the answer from the recursive function 'getString' and returns it
+        string ans = getString(index,"",nums,st);
+            
         return ans;
         
     }
