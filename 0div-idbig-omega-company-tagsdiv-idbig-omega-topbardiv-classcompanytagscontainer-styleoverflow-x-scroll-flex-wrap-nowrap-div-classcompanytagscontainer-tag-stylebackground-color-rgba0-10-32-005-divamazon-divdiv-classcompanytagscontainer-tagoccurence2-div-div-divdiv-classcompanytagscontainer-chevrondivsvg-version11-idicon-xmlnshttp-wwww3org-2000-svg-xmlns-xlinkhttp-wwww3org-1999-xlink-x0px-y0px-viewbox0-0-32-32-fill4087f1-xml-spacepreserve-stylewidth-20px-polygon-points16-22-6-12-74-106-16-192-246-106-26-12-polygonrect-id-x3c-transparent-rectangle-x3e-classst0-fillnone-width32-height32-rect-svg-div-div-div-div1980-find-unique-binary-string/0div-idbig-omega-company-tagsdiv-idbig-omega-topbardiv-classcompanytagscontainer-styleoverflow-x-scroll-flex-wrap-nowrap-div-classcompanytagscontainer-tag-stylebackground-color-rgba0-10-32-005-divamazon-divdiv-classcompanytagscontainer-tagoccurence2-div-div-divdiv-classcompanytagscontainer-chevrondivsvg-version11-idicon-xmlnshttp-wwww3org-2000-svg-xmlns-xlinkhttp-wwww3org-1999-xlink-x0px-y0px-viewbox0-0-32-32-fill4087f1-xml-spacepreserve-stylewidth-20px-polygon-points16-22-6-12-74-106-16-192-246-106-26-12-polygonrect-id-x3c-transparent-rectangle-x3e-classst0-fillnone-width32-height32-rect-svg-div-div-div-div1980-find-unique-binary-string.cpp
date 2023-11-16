a1711c0991a745bc195353d85a54f03542d64f0c
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2023-11-16 14:49:18 +0530

    Time: 17 ms (22.51%), Space: 24.4 MB (17.07%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1980-find-unique-binary-string/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1980-find-unique-binary-string.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1980-find-unique-binary-string/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1980-find-unique-binary-string.cpp
@@ -3,37 +3,44 @@ public:
     
     string getString(int index,string path, vector<string> nums,set<string> st){
         
-        //base case
+        //base case will be reached when the index exceeds the last index of nums
         if(index == nums.size()){
             
-            if(st.find(path) == st.end())
+            if(st.find(path) == st.end())//if the answer is found
                 return path;
-            else
+            else//if the answer is not found
                 return "";
             
         }
         
         //recursive calls
+        
+        //try taking 0 in the path and go for further calls---------------
         path.push_back('0');
         string str1 = getString(index+1,path,nums,st);
         
+        //if we cound find the answer, return that answer itself
         if(str1.size() > 0)
             return str1;
         
+        //if we cound'nt find the answer,remove 0 and try putting 1 inplace of 0
+        //and go for further calls
         path.pop_back();
-        
         path.push_back('1');
+        
         string str2 = getString(index+1,path,nums,st);
         
+        //if we cound find the answer, return that answer itself
         if(str2.size() > 0)
             return str2;
         
+        //if non of str1 or str2 gave the answer, return an empty string
         return "";
     }
     
     string findDifferentBinaryString(vector<string>& nums) {
         
-        //push all the elements to a set
+        //push all the elements to a hashSet so to check for the answer further
         set<string> st;
         for(string s: nums)
                 st.insert(s);
@@ -42,6 +49,7 @@ public:
         //and get the one which is not present in set
         int index = 0;
         
+        //this gets the answer from the recursive function 'getString' and returns it
         string ans = getString(index,"",nums,st);
             
         return ans;
