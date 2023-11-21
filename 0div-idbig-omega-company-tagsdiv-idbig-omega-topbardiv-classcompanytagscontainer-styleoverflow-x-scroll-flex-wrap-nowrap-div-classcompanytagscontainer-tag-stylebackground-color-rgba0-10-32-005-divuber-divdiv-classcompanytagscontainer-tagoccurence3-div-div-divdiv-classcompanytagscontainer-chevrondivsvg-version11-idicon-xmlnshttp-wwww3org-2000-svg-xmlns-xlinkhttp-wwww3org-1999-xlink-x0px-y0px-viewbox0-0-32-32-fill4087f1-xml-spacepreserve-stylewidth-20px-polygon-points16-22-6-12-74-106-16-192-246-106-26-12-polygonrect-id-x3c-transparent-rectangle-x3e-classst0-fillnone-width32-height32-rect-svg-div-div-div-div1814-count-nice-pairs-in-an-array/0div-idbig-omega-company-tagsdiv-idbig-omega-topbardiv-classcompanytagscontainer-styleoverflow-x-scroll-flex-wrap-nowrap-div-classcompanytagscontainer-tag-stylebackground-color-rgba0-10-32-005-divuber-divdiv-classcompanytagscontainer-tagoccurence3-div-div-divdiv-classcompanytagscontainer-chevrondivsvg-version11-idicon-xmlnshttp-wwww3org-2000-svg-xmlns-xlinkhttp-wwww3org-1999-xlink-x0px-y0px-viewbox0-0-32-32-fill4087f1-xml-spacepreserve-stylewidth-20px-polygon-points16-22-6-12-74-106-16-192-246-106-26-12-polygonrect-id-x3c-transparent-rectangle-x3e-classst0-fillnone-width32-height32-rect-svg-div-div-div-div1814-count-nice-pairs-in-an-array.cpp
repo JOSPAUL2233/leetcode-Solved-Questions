1a312f1e09f6a8e3b979e58d7f8ba075088bf21d
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2023-11-21 15:55:54 +0530

    Time: 103 ms (45.93%), Space: 67.2 MB (5.48%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1814-count-nice-pairs-in-an-array/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1814-count-nice-pairs-in-an-array.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1814-count-nice-pairs-in-an-array/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1814-count-nice-pairs-in-an-array.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     
+    //this function returns the reverse of n
     int reverse(int n){
         
         int rev = 0;
@@ -17,6 +18,7 @@ public:
         
     }
     
+    //this function reverse all the elements of nums and returns the same
     vector<int> getRev(vector<int> nums){
         
         vector<int> ans;
@@ -33,6 +35,7 @@ public:
     
     int countNicePairs(vector<int>& nums) {
         
+        //define a mod
         int mod = 1000000007;
         
         vector<int> rev;
@@ -40,22 +43,28 @@ public:
         map<int,long long> freq;
         
         rev = getRev(nums);
+        
+        //keep a count of all the pairs
         int count = 0;
         
         for(int i = 0;i<nums.size();i++){
             
+            //store the subtracted value of (nums - rev(nums))
             sub[i] = nums[i] - rev[i];
             
+            //count the number of pairs it can be formed.
             if(freq.find(sub[i]) != freq.end()){
                 
                 count = (count + freq[sub[i]]) % mod;
                 
             }
             
+            //increase the frequency of current number.
             freq[sub[i]]++;
             
         }
         
+        //return the count
         return count;
         
     }
