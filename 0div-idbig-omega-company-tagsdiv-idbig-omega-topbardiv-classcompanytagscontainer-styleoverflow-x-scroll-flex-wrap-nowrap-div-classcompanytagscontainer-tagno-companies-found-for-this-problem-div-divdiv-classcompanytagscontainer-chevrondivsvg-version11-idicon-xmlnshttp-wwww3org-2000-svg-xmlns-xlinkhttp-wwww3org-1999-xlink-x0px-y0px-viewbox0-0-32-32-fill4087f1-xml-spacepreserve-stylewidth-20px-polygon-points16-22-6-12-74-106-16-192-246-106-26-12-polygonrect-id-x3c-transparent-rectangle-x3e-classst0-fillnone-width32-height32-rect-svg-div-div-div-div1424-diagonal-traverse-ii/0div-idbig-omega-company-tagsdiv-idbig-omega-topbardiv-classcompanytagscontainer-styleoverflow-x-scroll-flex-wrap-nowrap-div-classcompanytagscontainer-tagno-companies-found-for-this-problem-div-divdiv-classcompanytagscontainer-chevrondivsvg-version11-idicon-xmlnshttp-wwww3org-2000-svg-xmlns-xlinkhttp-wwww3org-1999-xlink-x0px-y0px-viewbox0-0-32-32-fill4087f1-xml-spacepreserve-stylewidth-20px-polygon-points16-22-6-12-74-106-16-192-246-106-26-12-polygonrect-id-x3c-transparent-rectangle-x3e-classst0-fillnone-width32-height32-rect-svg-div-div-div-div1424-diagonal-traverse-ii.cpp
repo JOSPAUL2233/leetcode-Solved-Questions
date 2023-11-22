48748febda6099b9a3c61f297f31e12027c949fd
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2023-11-22 20:35:47 +0530

    Time: 112 ms (94.23%), Space: 65.9 MB (94.66%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1424-diagonal-traverse-ii/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1424-diagonal-traverse-ii.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1424-diagonal-traverse-ii/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1424-diagonal-traverse-ii.cpp
@@ -2,31 +2,43 @@ class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
         
+        queue<pair<int,int>> que;
         
-        unordered_map<int,vector<int>> mp;
+        que.push({0,0});
+        
         vector<int> ans;
         
-        int maxDiaSum = 0;
-        
-        for(int i = nums.size()-1;i>=0;i--){
+        while(!que.empty()){
             
-            for(int j = nums[i].size()-1;j>=0;j--){
+            
+            int size = que.size();
+            
+            for(int i = 0;i<size;i++){
                 
-                int diaSum = i+j;
-                mp[diaSum].push_back(nums[i][j]);
-                maxDiaSum = max(maxDiaSum,diaSum);
+                auto p = que.front();
+                que.pop();
+                                
+                int r = p.first;
+                int c = p.second;
+                
+                ans.push_back(nums[r][c]);
+                
+                //push bottom
+                if(r+1 < nums.size() && c == 0){
+                
+                    que.push({r+1,c});
+                    
+                }
+                
+                // push right
+                if(c+1 < nums[r].size()){
+                    
+                    que.push({r,c+1});
+                    
+                }
                 
             }
             
-        }
-        
-        for(int i = 0;i<=maxDiaSum;i++){
-            
-            for(int ele : mp[i]){//map[i] is a vector and ele is the individual elements in that vector
-                
-                ans.push_back(ele);
-                
-            }
             
         }
         
