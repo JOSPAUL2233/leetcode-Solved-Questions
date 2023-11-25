fc629926b66147d4874eeb9c276336dd7bb81d2f
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2023-11-25 14:55:40 +0530

    Time: 100 ms (28.11%), Space: 83.5 MB (73.78%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1685-sum-of-absolute-differences-in-a-sorted-array/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1685-sum-of-absolute-differences-in-a-sorted-array.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1685-sum-of-absolute-differences-in-a-sorted-array/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1685-sum-of-absolute-differences-in-a-sorted-array.cpp
@@ -4,34 +4,25 @@ public:
         
         int n = nums.size();
         
-        //maintain seperate prefix and suffix sum
-        vector<int> prefixSum(n,-1);
-        vector<int> suffixSum(n,-1);
+        //maintain a prefixSum and totalSum variable and from which we can also calculate suffixSum lateron
                 
-        int preSum = 0;
-        int sufSum = 0;
+        int totalSum = 0;
         
-        for(int i = 0;i<n;i++){
-            
-            prefixSum[i] = preSum;
-            preSum += nums[i];
-            
-            sufSum += nums[n-1-i];
-            suffixSum[n-1-i] = sufSum;
-            
-        }
+        for(int i : nums) totalSum += i;
         
-        //now, computer the answer seperately using prefix and suffix sum in calculation
-        //inorder to overcome negative values.
+        //now, computer the answer seperately using prefix and suffix sum
         
         vector<int> ans(n,-1);
-        
+        int prefixSum = 0;
+
         for(int i = 0;i<nums.size();i++){
             
             int leftCount = i;//number of elements present left side of ith position
             int rightCount = n-i;//number of elements present right side of ith position including the ith element
+            int suffixSum = totalSum - prefixSum;
             
-            ans[i] = ((nums[i] * leftCount) - prefixSum[i]) + (suffixSum[i] - ( nums[i] * rightCount));
+            ans[i] = ((nums[i] * leftCount) - prefixSum) + (suffixSum - ( nums[i] * rightCount));            
+            prefixSum += nums[i];
             
         }
         
