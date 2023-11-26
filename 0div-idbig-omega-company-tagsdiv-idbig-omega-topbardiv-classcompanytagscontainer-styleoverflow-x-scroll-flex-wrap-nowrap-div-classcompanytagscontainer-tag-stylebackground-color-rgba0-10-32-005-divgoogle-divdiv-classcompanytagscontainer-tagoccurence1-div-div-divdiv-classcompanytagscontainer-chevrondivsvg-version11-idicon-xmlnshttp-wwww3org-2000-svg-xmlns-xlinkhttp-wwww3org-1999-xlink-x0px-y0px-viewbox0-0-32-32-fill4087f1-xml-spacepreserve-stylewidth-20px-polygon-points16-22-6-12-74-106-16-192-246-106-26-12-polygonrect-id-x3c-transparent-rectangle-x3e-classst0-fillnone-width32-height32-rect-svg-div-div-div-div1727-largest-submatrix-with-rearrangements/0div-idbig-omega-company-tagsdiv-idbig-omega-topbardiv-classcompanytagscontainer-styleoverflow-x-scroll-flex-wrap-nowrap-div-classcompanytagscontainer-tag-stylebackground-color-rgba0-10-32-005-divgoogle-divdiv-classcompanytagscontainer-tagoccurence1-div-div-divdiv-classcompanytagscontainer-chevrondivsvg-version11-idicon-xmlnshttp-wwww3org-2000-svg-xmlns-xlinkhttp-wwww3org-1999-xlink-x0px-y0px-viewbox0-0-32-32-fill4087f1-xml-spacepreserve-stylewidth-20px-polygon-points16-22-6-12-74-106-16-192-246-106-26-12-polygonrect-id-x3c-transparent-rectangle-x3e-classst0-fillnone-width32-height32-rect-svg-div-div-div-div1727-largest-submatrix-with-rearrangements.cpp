a53446b5fbe73d9311400cad90b32f4c1afafdee
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2023-11-26 18:00:10 +0530

    Time: 203 ms (7.39%), Space: 96 MB (5.11%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1727-largest-submatrix-with-rearrangements/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1727-largest-submatrix-with-rearrangements.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1727-largest-submatrix-with-rearrangements/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1727-largest-submatrix-with-rearrangements.cpp
@@ -2,47 +2,60 @@ class Solution {
 public:
     int largestSubmatrix(vector<vector<int>>& matrix) {
         
-        vector<int> prev(matrix[0].size(),0);
-        vector<int> curr(matrix[0].size(),0);
-        // vector<int> sorted(matrix.size(),0);
+        int m = matrix.size();
+        int n = matrix[0].size();
         
         int maxArea = 0;
+
+        vector<pair<int, int>> prevHeights;
         
-        for(int i = 0;i<matrix.size();i++){
+        for (int r = 0; r < m; r++) {
+
+            vector<pair<int,int>> heights;
+            
+            //mark all the row elements as not visited
+            vector<bool> visited(n, false);
+
+            for (auto p : prevHeights) {
                 
-            //finding heights of curr row
-            for(int j = 0;j<matrix[0].size();j++){
-                                
-                if(matrix[i][j] == 0){
-                    curr[j] = 0;
-                }else{
+                int c = p.second;//gives col
+                int h = p.first;//gives height
+                
+                //update  if there is a valid height at r,c (ie.1)
+                if (matrix[r][c] == 1) {
                     
-                   if(i == 0){//first row
-                       curr[j] = 1;
-                   }else{
-                       curr[j] = 1 + prev[j];
-                   }
+                    //update the current height along with it's col and mark it as visited
+                    heights.push_back({h + 1, c});
+                    visited[c] = true;
                     
-                }                
+                }
                 
             }
-            
-            //store the current row in prev row
-            prev = curr;
-            
-            //extract the current row and sort it
-            sort(curr.begin(),curr.end());
 
-            //get the answer from the sorted row
-            for(int j = 0;j<curr.size();j++){
+            
+            for (int c = 0; c < n; c++) {
                 
-                int h = curr[j];
-                int w = curr.size() - j;
-                                
+                //update the height if it is not updated early and it is a valid height
+                if (!visited[c] && matrix[r][c] == 1) {
+                    
+                    heights.push_back({1, c});
+                    
+                }
+                
+            }
+			
+
+            for (int i = 0; i < heights.size(); i++) {
+                
+                int h = heights[i].first;
+                int w = i+1;
+
                 int area = h*w;
                 maxArea = max(maxArea,area);
                 
             }
+            
+            prevHeights = heights;
             
         }
 
