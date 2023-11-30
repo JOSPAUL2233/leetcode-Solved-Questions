c05fdde0ef0b75b76545bd93fa5b7bfebda9a15e
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2023-11-30 15:28:08 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (80.09%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1611-minimum-one-bit-operations-to-make-integers-zero/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1611-minimum-one-bit-operations-to-make-integers-zero.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    
+    int minimumOneBitOperations(int n) {
+        
+        long long f[31];
+        
+        f[0] = 1;
+        
+        for (int i = 1; i <= 30; i++) {
+            
+            f[i] = 2 * f[i - 1] + 1;
+            
+        }
+
+        int ans = 0;
+        int count = 0;
+        
+        for (int i = 30; i >= 0; i--) {
+            
+            if (((1 << i) & n) != 0) {
+                
+                count++;
+                
+                if (count % 2 == 1) {
+                    ans += f[i];
+                } else {
+                    ans -= f[i];
+                }
+                
+            }
+        }
+
+        return ans;
+    }
+};
