a9bc680efd7ca58665d8a906fceda4fc72f31b8b
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2023-12-01 13:34:34 +0530

    Time: 4 ms (57.15%), Space: 11.6 MB (99.56%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1662-check-if-two-string-arrays-are-equivalent/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1662-check-if-two-string-arrays-are-equivalent.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        
+        int s1 = 0;
+        int s2 = 0;
+        
+        int ptr1 = 0;
+        int ptr2 = 0;
+        
+        while(s1<word1.size() && s2<word2.size()){
+                        
+            if(word1[s1][ptr1] != word2[s2][ptr2])
+                return false;
+            
+            ptr1++;
+            ptr2++;
+            
+            if(ptr1 == word1[s1].size()){
+                
+                ptr1 = 0;
+                s1++;
+                
+            }
+                        
+            if(ptr2 == word2[s2].size()){
+                
+                ptr2 = 0;
+                s2++;
+                
+            }            
+            
+        }
+        
+        return (s1==word1.size()) && (s2==word2.size());
+        
+    }
+};
