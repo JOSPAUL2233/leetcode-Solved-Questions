9faae79316a24bcffffe7395bd04c37b23b5cd2a
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2023-12-11 15:02:18 +0530

    Time: 4 ms (95.90%), Space: 13.3 MB (44.46%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1287-element-appearing-more-than-25-in-sorted-array/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1287-element-appearing-more-than-25-in-sorted-array.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1287-element-appearing-more-than-25-in-sorted-array/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1287-element-appearing-more-than-25-in-sorted-array.cpp
@@ -1,14 +1,74 @@
 class Solution {
 public:
+    
+    int findLen(vector<int> arr,int target){
+        
+        int start = 0;
+        int end = arr.size()-1;
+        
+        int firstOccurance = -1;
+        
+        while(start <= end){
+            
+            int mid = start + (end-start)/2;
+            
+            if(arr[mid] == target){
+                
+                firstOccurance = mid;
+                
+            }
+            
+            if(arr[mid] >= target)
+                end = mid-1;
+            else
+                start = mid+1;
+
+        }
+        
+        int lastOccurance = -1;
+        start = 0;
+        end = arr.size()-1;
+        
+        while(start <= end){
+            
+            int mid = start + (end-start)/2;
+            
+            if(arr[mid] == target){
+                
+                lastOccurance = mid;
+                
+            }
+            
+            if(arr[mid] > target)
+                end = mid-1;
+            else
+                start = mid+1;
+
+        }
+        
+        return lastOccurance - firstOccurance + 1;
+        
+    }
+    
     int findSpecialInteger(vector<int>& arr) {
         
-        int reach = arr.size()/4;
+        int n = arr.size();
+        vector<int> indices = {n/4,n/2,3*n/4};
         
-        for(int i = 0;i<arr.size();i++){
+        int reach = n/4;
+        
+        int ans = -1;
+        
+        for(int i = 0;i<3;i++){
             
-            if(arr[i] == arr[i+reach])
-                return arr[i];
-                                
+            int len = findLen(arr,arr[indices[i]]);
+                        
+            if(len > reach){
+                
+                return arr[indices[i]];
+                
+            }
+            
         }
         
         return -1;
