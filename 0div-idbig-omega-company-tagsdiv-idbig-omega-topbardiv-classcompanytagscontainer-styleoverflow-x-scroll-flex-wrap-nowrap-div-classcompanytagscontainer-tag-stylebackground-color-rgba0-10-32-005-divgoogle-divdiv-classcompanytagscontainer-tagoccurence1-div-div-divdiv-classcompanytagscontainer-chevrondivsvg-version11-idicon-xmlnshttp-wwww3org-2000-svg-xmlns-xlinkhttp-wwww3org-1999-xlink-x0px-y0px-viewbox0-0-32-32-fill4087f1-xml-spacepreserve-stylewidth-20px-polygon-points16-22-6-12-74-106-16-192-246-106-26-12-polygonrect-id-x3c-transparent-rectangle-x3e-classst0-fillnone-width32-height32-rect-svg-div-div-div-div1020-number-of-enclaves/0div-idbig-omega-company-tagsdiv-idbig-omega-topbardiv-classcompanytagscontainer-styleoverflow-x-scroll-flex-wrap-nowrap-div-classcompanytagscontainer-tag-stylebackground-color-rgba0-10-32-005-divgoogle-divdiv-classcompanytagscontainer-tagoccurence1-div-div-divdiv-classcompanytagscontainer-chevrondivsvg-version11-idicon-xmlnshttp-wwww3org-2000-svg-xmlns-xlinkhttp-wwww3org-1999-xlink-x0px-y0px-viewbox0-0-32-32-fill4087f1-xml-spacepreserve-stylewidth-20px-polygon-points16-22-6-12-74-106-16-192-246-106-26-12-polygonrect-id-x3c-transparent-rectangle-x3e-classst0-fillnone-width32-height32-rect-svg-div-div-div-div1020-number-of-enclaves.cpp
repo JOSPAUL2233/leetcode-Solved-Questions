3977a94d7c2684905eff25702a73ac1f0f083bbc
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2024-01-04 17:48:26 +0530

    Time: 58 ms (60.21%), Space: 29.5 MB (62.09%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1020-number-of-enclaves/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1020-number-of-enclaves.cpp
@@ -0,0 +1,84 @@
+class Solution {
+public:
+    
+    vector<int> delRow = {-1,0,+1,0};
+    vector<int> delCol = {0,-1,0,+1};
+    
+    void dfsSol(vector<vector<int>> &grid,int r,int c){
+        
+        int row = grid.size();
+        int col = grid[0].size();
+        
+        grid[r][c] = 0;
+        
+        //for for 4-neibours
+        for(int i = 0;i<4;i++){
+            
+            int nRow = r+delRow[i];
+            int nCol = c+delCol[i];
+            
+            if(nRow>=0 && nRow<row && nCol>=0 && nCol<col && grid[nRow][nCol] == 1){
+                
+                dfsSol(grid,nRow,nCol);
+                
+            }
+            
+        }
+        
+    }
+    
+    int numEnclaves(vector<vector<int>>& grid) {
+        
+        int row = grid.size();
+        int col = grid[0].size();
+        
+        // vector<vector<int>> visited(row,vector<int>(col,0));
+        
+        //can be solved using BFS or DFS
+        
+        //take care of top and bottom boundary
+        for(int i = 0;i<col;i++){
+            
+            //top
+            if(grid[0][i] == 1)
+                dfsSol(grid,0,i);
+            
+            //bottom
+            if(grid[row-1][i] == 1)
+                dfsSol(grid,row-1,i);
+            
+            
+        }
+        
+        //take care of left and right boundary
+        for(int i = 0;i<row;i++){
+            
+            //left
+            if(grid[i][0] == 1)
+                dfsSol(grid,i,0);
+            
+            //right
+            if(grid[i][col-1] == 1)
+                dfsSol(grid,i,col-1);
+            
+        }
+        
+        //count the number of landcells which is not visited
+        
+        int count = 0;
+        
+        for(int i = 0;i<row;i++){
+            
+            for(int j = 0;j<col;j++){
+                
+                if(grid[i][j] == 1)
+                    count++;
+                
+            }
+            
+        }
+        
+        return count;
+        
+    }
+};
