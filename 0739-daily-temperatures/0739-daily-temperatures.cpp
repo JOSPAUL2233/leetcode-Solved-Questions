0df1cc440d8bd43efc2f08789eadcd99e015c994
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2024-02-01 19:43:29 +0530

    Time: 100 ms (98.91%), Space: 100.9 MB (69.14%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -2,27 +2,36 @@ class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
         
-        //apply next greater element approach
-        stack<int> st;
-        vector<int> ans(temperatures.size(),0);
+        int n = temperatures.size();
+        vector<int> ans(n,0);
         
-        for(int i = temperatures.size()-1;i>=0;i--){
+        int highest = -1;
+        
+        for(int i = n-1;i>=0;i--){
             
-            //find the next greater element
-            while(!st.empty() && temperatures[st.top()] <= temperatures[i]){
+            //check if the element itself is the highest,
+            //which means, there is no highest value to the right of that element
+            if(temperatures[i] >= highest){
                 
-                st.pop();
+                highest = temperatures[i];
+                ans[i] = 0;
+            
+            }else{
+                
+                //keep a gap of 1 day at first
+                int days = 1;
+                
+                while(temperatures[i+days] <= temperatures[i]){
+                    
+                    //then go to next greater value
+                    days += ans[i+days];
+                    
+                }
+                
+                //store the next greater value
+                ans[i] = days;
                 
             }
-            
-            if(!st.empty()){
-                
-                //store the difference
-                ans[i] = st.top() - i;
-                
-            }
-            
-            st.push(i);
             
         }
         
