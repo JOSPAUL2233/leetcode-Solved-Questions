4f120f8f547e32834861b1847d1a5d1a8bef4458
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2024-02-17 18:52:24 +0530

    Time: 89 ms (25.63%), Space: 57.2 MB (53.87%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -1,57 +1,35 @@
 class Solution {
 public:
-    
-    bool isPossible(vector<int> heights,int bricks,int ladders,int mid){
+    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
         
-        vector<int> vec;
-                
-        for(int i = 1;i<=mid;i++){
+        priority_queue<int,vector<int>, greater<int>> pq;//MIN HEAP
+        
+        for(int i = 1;i<heights.size();i++){
             
             int diff = heights[i] - heights[i-1];
             
-            if(diff > 0)
-                vec.push_back(diff);
-            
-        }
-        
-        sort(vec.begin(),vec.end());
-        
-        for(int i : vec){
-            
-            if(i<=bricks){
-                bricks -= i;
-            }else if(ladders > 0){
-                ladders--;
-            }else{
-                return false;
+            if(diff > 0){
+                
+                pq.push(diff);
+                
+                if(pq.size() > ladders){
+                    
+                    int ele = pq.top();
+                    pq.pop();
+                                        
+                    if(bricks >= ele){
+                        bricks -= ele;                        
+                    }else{
+                        return i-1;
+                    }
+                    
+                }
+                
             }
             
         }
         
-        return true;
-        
-    }
-    
-    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
-        
-        int start = 0;
-        int end = heights.size()-1;
-        int ans = -1;
-        
-        while(start <= end){
-            
-            int mid = start + (end-start)/2;
-            
-            if(isPossible(heights,bricks,ladders,mid)){
-                ans = mid;
-                start = mid+1;
-            }else{
-                end = mid-1;
-            }
-            
-        }
-        
-        return ans;
+        return heights.size()-1;
         
     }
 };
