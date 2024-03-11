1ed0b5cff2f40a4cd5dbed2e6b181bbeceaaf7b5
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2024-03-12 04:07:53 +0530

    Time: 15 ms (69.64%), Space: 16.3 MB (87.66%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,16 +1,50 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& a, int src, int sink, int k) {
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         
-        vector<int> c(n, 1e8);
-        c[src] = 0;
-        
-        for(int z=0; z<=k; z++){
-            vector<int> C(c);
-            for(auto e: a)
-                C[e[1]] = min(C[e[1]], c[e[0]] + e[2]);
-            c = C;
+        //make adj lists
+        vector<pair<int,int>> adj[n];
+        for(int i = 0;i<flights.size();i++){
+            
+            adj[flights[i][0]].push_back({flights[i][1],flights[i][2]});
+            
         }
-        return c[sink] == 1e8 ? -1 : c[sink];
+        
+        vector<int> minDist(n,INT_MAX);
+        
+        queue<pair<int,pair<int,int>>> que;
+        que.push({0,{src,0}}); //cost,node,steps
+        
+        minDist[src] = 0;
+        
+        while(!que.empty()){
+            
+            auto p = que.front();
+            que.pop();
+            
+            int cost = p.first;
+            int node = p.second.first;
+            int stops = p.second.second;
+            
+            // cout<<cost<<" "<<node<<" "<<steps<<endl;
+            
+            if(stops > k)
+                continue;
+                        
+            for(auto it : adj[node]){
+                
+                if(cost + it.second < minDist[it.first]){
+                    minDist[it.first] = cost+it.second;
+                    que.push({cost+it.second,{it.first,stops+1}});
+                }
+                
+            }
+            
+        }
+        
+        if(minDist[dst]==INT_MAX)
+            return -1;
+        else
+            return minDist[dst];
     }
 };
