03331af555b6a8c7490cce4d10b5a4fd9014342b
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2024-03-13 03:17:44 +0530

    Time: 60 ms (49.70%), Space: 36 MB (58.08%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
@@ -3,30 +3,50 @@ public:
     int countPaths(int n, vector<vector<int>>& roads) {
         
         vector<pair<int,int>>adj[n];
+        
         for(auto it:roads){
+            
             adj[it[0]].push_back({it[1],it[2]});
             adj[it[1]].push_back({it[0],it[2]});
+            
         }
+        
         priority_queue<pair<long long ,int>,vector<pair<long long,int>>,greater<pair<long long,int>>>pq;
+        
         pq.push({0,0});
-        vector<long long >dis(n,1e15),ways(n,0);
+        
+        //maintain a dist(minDist) array and ways(number of ways to reach at that node in shortest dist) array
+        vector<long long >minDist(n,1e15),ways(n,0);
         ways[0]=1;
-        dis[0]=0;
+        minDist[0]=0;
+        
         int mod=(int)1e9+7;
+        
         while(!pq.empty()){
-            int node=pq.top().second;
-            long long dist=pq.top().first;
+            
+            int node = pq.top().second;
+            long long dist = pq.top().first;
+            
             pq.pop();
+            
             for(auto it:adj[node]){
+                
                 int adjNode=it.first;
                 long long adjW=it.second;
-                if(dist+adjW < dis[adjNode]){
-                    dis[adjNode]=dist+adjW;
+                
+                //found new shortest path
+                if(dist+adjW < minDist[adjNode]){
+                    
+                    minDist[adjNode]=dist+adjW;
                     pq.push({dist+adjW,adjNode});
-                    ways[adjNode]=ways[node];
-                }
-                else if(dist + adjW ==dis[adjNode]){
-                    ways[adjNode]=(ways[adjNode]+ways[node])%mod;
+                    
+                    ways[adjNode]=ways[node];//store the number of ways
+                    
+                }else if(dist + adjW ==minDist[adjNode]){
+                    
+                    //found one more shortest path
+                    ways[adjNode] = (ways[adjNode]+ways[node])%mod;//add up number of ways
+                    
                 }
             }
         }
