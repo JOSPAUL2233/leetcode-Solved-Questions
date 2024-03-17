e455c5569f79888005ef900fde54c00ee3a7b6f5
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2024-03-17 21:11:38 +0530

    Time: 21 ms (8.90%), Space: 23 MB (5.24%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        
+        //insert newInterval at the appropriate place
+        
+        vector<vector<int>> vec;
+        
+        for(auto it : intervals){
+            
+            if(it[0] <= newInterval[0])
+                vec.push_back(it);
+            else
+                break;
+            
+            
+        }
+        
+        vec.push_back(newInterval);
+        
+        for(auto it : intervals){
+            
+            if(it[0] > newInterval[0])
+                vec.push_back(it);
+            
+        }
+        
+        //merge the intervals if needed
+        vector<vector<int>> ans;
+        pair<int,int> p = {vec[0][0],vec[0][1]};
+        
+        for(auto it : vec){
+            
+            if(it[0] <= p.second){//if overlaps
+                
+                p.second = max(p.second,it[1]);
+                
+            }else{
+                
+                ans.push_back({p.first,p.second});
+                p = {it[0],it[1]};
+                
+            }
+            
+        }
+        
+        ans.push_back({p.first,p.second});
+        
+        return ans;
+        
+    }
+};
