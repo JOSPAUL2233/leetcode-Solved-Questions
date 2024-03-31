8f20a0883e2cab18ef31c00a76c1b745e78c32f6
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2024-03-31 15:04:15 +0530

    Time: 76 ms (94.68%), Space: 82.6 MB (53.99%) - LeetHub

--- 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
+++ 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
@@ -4,23 +4,23 @@ public:
         
         long long res = 0;
         
-        int bad_idx = -1, left_idx = -1, right_idx = -1;
+        int b = -1, li = -1, ri = -1;
 
         for (int i = 0; i < nums.size(); ++i) {
             
             if (!(minK <= nums[i] && nums[i] <= maxK)) {
-                bad_idx = i;
+                b = i;
             }
 
             if (nums[i] == minK) {
-                left_idx = i;
+                li = i;
             }
 
             if (nums[i] == maxK) {
-                right_idx = i;
+                ri = i;
             }
             
-            res += max(0, min(left_idx, right_idx) - bad_idx);
+            res += max(0, min(li, ri) - b);
         }
 
         return res;
