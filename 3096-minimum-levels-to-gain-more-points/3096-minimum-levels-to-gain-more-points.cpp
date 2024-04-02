a061d579e18d79f69bcdd6940982b3b378182402
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2024-04-02 20:51:30 +0530

    Time: 231 ms (38.47%), Space: 260.4 MB (65.95%) - LeetHub

--- 3096-minimum-levels-to-gain-more-points/3096-minimum-levels-to-gain-more-points.cpp
+++ 3096-minimum-levels-to-gain-more-points/3096-minimum-levels-to-gain-more-points.cpp
@@ -3,34 +3,30 @@ public:
     int minimumLevels(vector<int>& possible) {
         
         int n = possible.size();
-        
-        vector<int> prefix(possible.size(),0);
-        vector<int> suffix(possible.size(),0);
-        
-        int pre = 0;
-        int suf = 0;
+            
+        int totalPoints = 0;
         
         for(int i = 0;i<n;i++){
             
-            if(possible[i]==1)
-                pre++;
+            if(possible[i] == 1)
+                totalPoints++;
             else
-                pre--;
+                totalPoints--;
             
-            if(possible[n-1-i]==1)
-                suf++;
-            else
-                suf--;
-            
-            prefix[i] = pre;
-            suffix[n-1-i] = suf;
         }
         
         //iterate
         
+        int preSum = 0;
+        
         for(int i = 0;i<n-1;i++){
             
-            if(prefix[i] > suffix[i+1])
+            if(possible[i] == 1)
+                preSum++;
+            else
+                preSum--;
+            
+            if(preSum > (totalPoints-preSum))
                 return i+1;
             
         }
