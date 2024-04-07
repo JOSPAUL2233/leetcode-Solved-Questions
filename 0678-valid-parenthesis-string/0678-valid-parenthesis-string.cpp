fa274f7ce6c66734dd880455ed2773ff8495de53
Jos Paul <81140899+JOSPAUL2233@users.noreply.github.com>
2024-04-07 18:28:31 +0530

    Time: 3 ms (45.50%), Space: 7.4 MB (39.07%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -2,24 +2,29 @@ class Solution {
 public:
     bool checkValidString(string s) {
         
+        //maintain seperate brackets for opening brackets and stars
         stack<int> open,star;
         
         for(int i = 0;i<s.size();i++){
             
             if(s[i] == '*'){
                 
+                //if it's a star, simply push it into star stack
                 star.push(i);
-                
-                
+
             }else if(s[i] == '('){
-                    
-                    open.push(i);
+
+                //if it's an opening bracket, simply push it into opening stack  
+                open.push(i);
                     
             }else{
-                    
-                    if(open.size() > 0){
+                
+                    //if it's closing bracket, check in opening stack first, then in star stack
+                    //if nothing can cancel it out, then it's an invalid string
+                
+                    if(!open.empty()){
                         open.pop();
-                    }else if(star.size() > 0){
+                    }else if(!star.empty()){
                         star.pop();
                     }else{
                         return false;
@@ -29,13 +34,16 @@ public:
                 
         }
         
-        while(open.size() > 0 && star.size() > 0 && open.top() < star.top()){
+        //if there are some opening brackets left, make sure that starts can cancel them out by checking
+        //that the starts are coming after each opening brackets, so comparing the indices
+        
+        while(!open.empty() && !star.empty() && open.top() < star.top()){
             open.pop();
             star.pop();
         }
 
-        
-        return open.size()==0;        
+        //if the stack is empty, then it's valid otherwise it's invalid
+        return open.empty();        
                 
             
     }
